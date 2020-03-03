27c31e135b8b6fbe50414033f4aa0e63b86b5525
Daniel Boucher <dtboucher@gmail.com>
2020-03-03 23:42:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+// * Create a variable with an empty array.
+var solution = [];
+let musicians = [John, Paul, George, Ringo];
+let instuments = [Guitar, Piano, Bass, Drums];
+
+function
+for (var musicians = 0; i <= length.musicians - 1; musicians++);
+console.log(musicians);
+
+// * Use a `for loop`, which iterates over the array of musicians. Be careful
+//   about what value you set your `counter` variable to store. (Hint: Think
+//   about what the first index of an array is).
+// * The first time through the loop, it should create a string using the _first_
+//   index of the musicians array **and** the first index of the instruments
+//   array: `"John Lennon plays guitar"`.
+// * Add this string to the empty array you created. The loop should make the
+//   same sentence for every member of the musicians array.
+// * The function should return the array of new strings containing what
+//   instruments each musician plays.
